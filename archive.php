94b39168fc3e4c95a8caa700d4e8383345283bee
Ryan Lieu <github-benzBrake@woai.ru>
2024-08-02 23:14:25 +0800

    fix: 修复多处问题

--- archive.php
+++ archive.php
@@ -4,42 +4,25 @@
 <div class="col-mb-12 col-8" id="main" role="main">
     <h3 class="archive-title"><?php $this->archiveTitle([
             'category' => _t('分类 %s 下的文章'),
-            'search'   => _t('包含关键字 %s 的文章'),
-            'tag'      => _t('标签 %s 下的文章'),
-            'author'   => _t('%s 发布的文章')
+            'search' => _t('包含关键字 %s 的文章'),
+            'tag' => _t('标签 %s 下的文章'),
+            'author' => _t('%s 发布的文章')
         ], '', ''); ?></h3>
-    <?php if ($this->have()): ?>
-        <?php while ($this->next()): ?>
-            <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
-                <h2 class="post-title" itemprop="name headline"><a itemprop="url"
-                                                                   href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
-                </h2>
-                <ul class="post-meta">
-                    <li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a
-                            itemprop="name" href="<?php $this->author->permalink(); ?>"
-                            rel="author"><?php $this->author(); ?></a></li>
-                    <li><?php _e('时间: '); ?>
-                        <time datetime="<?php $this->date('c'); ?>"
-                              itemprop="datePublished"><?php $this->date(); ?></time>
-                    </li>
-                    <li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
-                    <li itemprop="interactionCount"><a
-                            href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a>
-                    </li>
-                </ul>
-                <div class="post-content" itemprop="articleBody">
-                    <?php $this->content('- 阅读剩余部分 -'); ?>
-                </div>
-            </article>
-        <?php endwhile; ?>
-    <?php else: ?>
-        <article class="post">
-            <h2 class="post-title"><?php _e('没有找到内容'); ?></h2>
-        </article>
-    <?php endif; ?>
-
+    <ul class="post-list">
+        <?php if ($this->have()): ?>
+            <?php while ($this->next()): ?>
+                <li class="post relative" itemscope itemtype="http://schema.org/BlogPosting">
+                    <a itemprop="url" href="<?php $this->permalink() ?>">
+                        <span class="date"><?php $this->date('Y-m-d'); ?></span>
+                        <span class="spliter">»</span>
+                        <span class="title" itemprop="name headline"><?php $this->title() ?></span>
+                    </a>
+                </li>
+            <?php endwhile; ?>
+        <?php else: ?>
+            <div class="text-[2.5em]"><?php _e("They're in another queue!") ?></div>
+        <?php endif; ?>
+    </ul>
     <?php $this->pageNav('&laquo; 前一页', '后一页 &raquo;'); ?>
 </div><!-- end #main -->
-
-<?php $this->need('sidebar.php'); ?>
 <?php $this->need('footer.php'); ?>
--- page.php
+++ page.php
@@ -13,6 +13,4 @@
     </article>
     <?php $this->need('comments.php'); ?>
 </div><!-- end #main-->
-
-<?php $this->need('sidebar.php'); ?>
 <?php $this->need('footer.php'); ?>
